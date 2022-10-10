c28dfb3d5f0c6499451d4fc4acc3a7bcbbdf393f
Personal-IliyanDimov <ilianmd@abv.bg>
2022-10-10 16:05:00 +0300

    initial project structure

---
+++ src/main/java/org/imd/release/ReleaseProjectApplication.java
@@ -0,0 +1,13 @@
+package org.imd.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/imd/release/ReleaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imd.release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
